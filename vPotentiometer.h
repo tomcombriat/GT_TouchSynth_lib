036e2e588212192f1750409879235b2dc45c5a42
tomcombriat <tomcombriat@live.fr>
2023-03-12 22:19:49 +0100

    Added text to vPotentiometer

--- vPotentiometer.h
+++ vPotentiometer.h
@@ -6,6 +6,11 @@
  */
 
 
+#define INNER_DISC 220
+#define INDICATOR_LENGTH 210
+#define TEXT_BASE_SIZE 6
+//#define MAX_STRING_LENGTH 10
+
 
 #include <Oscil.h>   // Mozzi
 #include <tables/sin256_int8.h>   // Mozzi
@@ -30,6 +35,7 @@ class vPotentiometer
   void setSize(int16_t _size) {
     old_size = size;
     size = _size;
+    max_string_length = (size<<1)/TEXT_BASE_SIZE;
   }
 
   /** Set the position of the visual potentiometer
@@ -72,36 +78,70 @@ class vPotentiometer
   */
   void setBackgroundColor(uint16_t _color)  {background_color = _color;}
 
+  void setText(String _text) {
+    refresh_text = true;
+    	if (_text.length() > max_string_length)
+	  {
+	    text = _text.substring(0,max_string_length-1) + ".";
+	  }
+	else text = _text;
+  }
+  
 
   /** Update the display of the visual potentiometer if needed
       @note Timing: around 5-10ms
   */
   void update()
   {
-    /////// Erase of previous one (when needed)
+    /// TODO: PUT REPETITIONS IN FUNCTION
+    if (old_size != size)
+      {
+	screen->fillCircle(old_pos_X, old_pos_Y, old_size, background_color);
+	screen->fillRect(old_pos_X - ((max_string_length*TEXT_BASE_SIZE)>>1), old_pos_Y + ((size*700)>>9), max_string_length*TEXT_BASE_SIZE, TEXT_BASE_SIZE+1,background_color);
+	refresh_text = true;
+	screen->fillCircle(pos_X, pos_Y, size, color);	
+	screen->fillCircle(pos_X, pos_Y, (size*INNER_DISC) >> 8, background_color);
+	drawLineAngle(pos_X,pos_Y,value,(size*INDICATOR_LENGTH)>>8,color);
+	old_pos_X = pos_X;
+	old_pos_Y = pos_Y;
+	old_value = value;
+	old_size = size;
+      }
     if (old_pos_X != pos_X || old_pos_Y != pos_Y)
       {
 	screen->fillCircle(old_pos_X, old_pos_Y, size, background_color);
 	screen->fillCircle(pos_X, pos_Y, size, color);
-	screen->fillCircle(pos_X, pos_Y, (size*220) >> 8, background_color);
-	drawLineAngle(pos_X,pos_Y,value,(size*210)>>8,color);
+	screen->fillCircle(pos_X, pos_Y, (size*INNER_DISC) >> 8, background_color);
+	drawLineAngle(pos_X,pos_Y,value,(size*INDICATOR_LENGTH)>>8,color);
 	old_pos_X = pos_X;
 	old_pos_Y = pos_Y;
 	old_value = value;
       }
     else if (old_value != value)
       {
-	Serial.println("c1");
-	drawLineAngle(pos_X,pos_Y,old_value,(size*210)>>8,background_color);
-	drawLineAngle(pos_X,pos_Y,value,(size*210)>>8,color);
+	drawLineAngle(pos_X,pos_Y,old_value,(size*INDICATOR_LENGTH)>>8,background_color);
+	drawLineAngle(pos_X,pos_Y,value,(size*INDICATOR_LENGTH)>>8,color);
 	old_value = value;	     
       }
     else if (old_color != color)
       {
 	screen->fillCircle(pos_X, pos_Y, size, color);
-	screen->fillCircle(pos_X, pos_Y, (size*220) >> 8, background_color);
-	drawLineAngle(pos_X,pos_Y,value,(size*210)>>8,color);
+	screen->fillCircle(pos_X, pos_Y, (size*INNER_DISC) >> 8, background_color);
+	drawLineAngle(pos_X,pos_Y,value,(size*INDICATOR_LENGTH)>>8,color);
+	screen->setCursor(pos_X - (text.length()*(TEXT_BASE_SIZE>>1)), pos_Y + ((size*700)>>9));
+	screen->setTextColor(color);
+	screen->print(text);
 	old_color = color;
+      }
+
+    if (refresh_text)
+      {
+	screen->fillRect(pos_X - ((max_string_length*TEXT_BASE_SIZE)>>1), pos_Y + ((size*700)>>9), max_string_length*TEXT_BASE_SIZE, TEXT_BASE_SIZE+1,background_color);
+	  
+	screen->setCursor(pos_X - (text.length()*(TEXT_BASE_SIZE>>1)), pos_Y + ((size*700)>>9));
+	screen->setTextColor(color);
+	screen->print(text);
+	refresh_text = false;
       }
   }
 
@@ -113,9 +153,12 @@ class vPotentiometer
   uint16_t color, old_color, background_color;
   uint8_t value;
   uint8_t old_value;
+  uint8_t max_string_length;
   static const byte NBit=8;
   Adafruit_ILI9341 * screen;
   bool visible;
+  String text;
+  bool refresh_text;
 
   void drawLineAngle(int16_t x0,int16_t  y0,uint8_t angle,int16_t length,uint16_t color)
   {
