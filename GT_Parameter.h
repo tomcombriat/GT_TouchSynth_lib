b89978ff0ba590633f03eabb896a86305b1630c9
tomcombriat <tomcombriat@live.fr>
2024-06-12 00:11:33 +0200

    Implemented monotonic MIDI input for parameter

--- GT_Parameter.h
+++ GT_Parameter.h
@@ -22,19 +22,15 @@ class GT_Parameter
   */
   inline int32_t getValue() {return value;}
 
-    /**
+  /**
      Set the value of the parameter
-@param _value: the value to input
-@param NBits_input the number of bits of the input
+     @param _value: the value to input
+     @param NBits_input the number of bits of the input
   */
   inline void setValue(int32_t _value, int8_t NBits_input) // assumes unsigned
   {
-    if (signedd)
-      {
-	 _value = GT_shiftR(_value, NBits_input-NBits);
-	_value -= 1<<(NBits-1);	
-      }
-    else  _value = GT_shiftR(_value, NBits_input-NBits);
+    _value = GT_shiftR(_value, NBits_input-NBits);
+    if (signedd) _value -= 1<<(NBits-1);	
     value = _value;
   }
 
@@ -69,7 +65,7 @@ class GT_Parameter
   */
   inline byte getMidiControl2() {return midi_control2;}
 
-    /**
+  /**
      Set the midi channel the parameter is watching
   */
   inline void setMidiChannel(byte _midi_channel) {midi_channel=_midi_channel;}
@@ -84,7 +80,51 @@ class GT_Parameter
   */
   inline void setMidiControl2(byte _control) {midi_control2=_control;}
 
+  /**
+     Notify of a MIDI input control change
+  */
+  inline void notifyMIDI(byte _channel, byte _control, byte _value)
+  {
+    if (_channel == midi_channel)
+      {
+	if (_control == midi_control1) // MSB
+	  {
+	    if (midi_control2 == 255) setValue(_value, 7); // no LSB
+	    
+	    else // LSB present
+	      {
+		/* This is to avoid weird steppin with HQ Midi:
+		   from below, we put the LSB to 0 (LSB might come higher, but that makes a monotonic transition) whereas coming from above we do the opposite
+		*/
+		int32_t scaled_value = GT_shiftR((int32_t)_value, 7 - NBits);
+		if (signedd) value += 1<<(NBits-1);
+		int32_t masked_old_value = GT_shiftR(127, 7 - NBits) & value;  // keep only the MSB
+		if (scaled_value > masked_old_value)  value = scaled_value;  
+		else if (scaled_value < masked_old_value) value = scaled_value + (GT_shiftR((int32_t)1, 7 - NBits)-1);
+		else value = scaled_value + ((GT_shiftR(1,7-NBits)-1) & value); // all this to take into account double send of the same midi signal
+		// ((GT_shiftR(1,7-NBits)-1) & value) is keeping only the LSB, everything else at 0
 
+		/* Simplified version (for non doublon)
+		   if (scaled_value > masked_old_value)  value = scaled_value;
+		   else value = scaled_value + (GT_shiftR((int32_t)1, 7 - NBits)-1);
+		*/
+		//else value = scaled_value + (GT_shiftR((int32_t)1, 7 - NBits)-1);
+		if (signedd) value -= 1<<(NBits-1);	    
+	      }  // else
+	    
+	  } // 	if (_control == midi_control1)
+	else if (_control == midi_control2)
+	  {
+	    int32_t scaled_value = GT_shiftR((int32_t)_value, 14 - NBits);
+	    if (signedd) value += 1<<(NBits-1);
+	    value = (GT_shiftR(127, 7 - NBits) & value) + scaled_value;
+	    //      ONLY THE MSB
+	    if (signedd) value -= 1<<(NBits-1);
+	  }  // else if (_control == midi_control2)
+      } // if (_channel == midi_channel)
+
+
+  }
   
    
 
@@ -97,7 +137,7 @@ class GT_Parameter
   int32_t value;
   const int32_t max_value=(signedd ? 1<<(NBits-1) : 1<<NBits)-1;
   const int32_t min_value=(signedd ? -1<<(NBits-1):0);
-  byte midi_channel, midi_control1, midi_control2;
+  byte midi_channel, midi_control1=255, midi_control2=255; // 255 is non active
   
 };
 
