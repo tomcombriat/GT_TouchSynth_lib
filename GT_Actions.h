7ae066bfb6ce30317c64076da43a0d53d64a774e
tomcombriat <tomcombriat@live.fr>
2024-08-10 16:44:22 +0200

    Added a randomize action

--- GT_Actions.h
+++ GT_Actions.h
@@ -1,6 +1,9 @@
 #ifndef _GT_ACTION_
 #define _GT_ACTION_
 
+#include<mozzi_rand.h>
+#include"GT_Parameter.h"
+
 /**
 This file defines a set of *actions* that can be triggered.
 They rely (for now) on a set of global variables being defined in the 
@@ -18,6 +21,16 @@ inline void disconnectAllParam()
     {
       if (allParams[i]) allParams[i]->disconnectInput(); // check if not nullptr
    }
+
+}
+
+inline void randomizeAllParam()
+{
+  for (byte i=0;i<N_PARAM;i++)
+    {
+      if (allParams[i]) allParams[i]->setValue(rand(65536),16);
+
+	}
 
 }
 
